42d5162e44f97ba90ae560847f09dfff676baad6
alpharaoh <alpharaohh@gmail.com>
2023-03-04 15:35:35 +0000

    Attempt to perform actions on opened browser

--- visa_vfs_api/__init__.py
+++ visa_vfs_api/__init__.py
@@ -4,4 +4,5 @@ from selenium.webdriver.chrome.options import Options
 
 chr_options = Options()
 chr_options.add_experimental_option("detach", True)
+# chr_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 driver = webdriver.Chrome(options=chr_options)
